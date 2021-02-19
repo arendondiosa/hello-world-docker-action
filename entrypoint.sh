f9e1007de01400f02c1ad450dda22ca7c44bd24e
Alejandro E. Rendon <alejandro@rendon.co>
2021-02-19 08:17:37 -0500

    Fixed job

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh -l
 
+git fetch origin $1
+
 number_of_commits=$(git rev-list --count HEAD ^$1)
 echo "Number of commits: $number_of_commits"
-
-git fetch origin $1
 
 darker --diff -r HEAD~$number_of_commits .
