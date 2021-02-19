eb5efe2ead87def3d225f798c47048bf8b675805
Alejandro E. Rendon <alejandro@rendon.co>
2021-02-19 08:44:52 -0500

    Fixed job

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,3 +6,4 @@ number_of_commits=$(git rev-list --count HEAD ^$1)
 echo "Number of commits: $number_of_commits"
 
 darker --diff -r HEAD~$number_of_commits .
+darker --check -r HEAD~$number_of_commits .
