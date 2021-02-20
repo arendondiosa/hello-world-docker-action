2cbef4b623991818a6c237ce4c9bbd464711eaf1
Alejandro E. Rendon <alejandro@rendon.co>
2021-02-19 20:31:15 -0500

    Custom darker command

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,4 @@ git branch $1 origin/$1
 number_of_commits=$(git rev-list --count HEAD ^$1)
 echo "Number of commits: $number_of_commits"
 
-darker --diff -r HEAD~$number_of_commits .
-darker --check -r HEAD~$number_of_commits .
+darker --diff --check -r HEAD~$number_of_commits .
